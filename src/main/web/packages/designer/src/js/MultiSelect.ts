dfa8e9150009223935efa5530ce61294a88af7c9
xima-awa <awa@xima.de>
2025-06-05 12:36:27 +0200

    Do not use deprecated property in MultiSelect + fix getValue method

--- src/main/web/packages/designer/src/js/MultiSelect.ts
+++ src/main/web/packages/designer/src/js/MultiSelect.ts
@@ -1,24 +1,28 @@
 import { $, Callbacks, Editors, type IPropertyDescriptor, type TEditorCfg } from "@de-xima/fc-form-designer";
+import { parseString } from "@de-xima/xima-common-js-lang";
+
 /** Defines the type of {@link MultiSelect }. */
 export const MultiSelectType = "com.github.xima_formcycle_entwicklerkreis.fc.plugin:fc-plugin-codbi:MultiSelect";
+
 /** Describes the {@link MultiSelectType }. */
-export interface IMultiSelectDescriptor extends IPropertyDescriptor<typeof MultiSelectType> {
-  /** Stores the selected standard configurations */
-  property: string;
-}
+export interface IMultiSelectDescriptor extends IPropertyDescriptor<typeof MultiSelectType> {}
+
 /** Augmenting **@de-xima/fc-form-designer** in order to add the {@link MultiSelectType } to {@link IEditorMap }.*/
 declare module "@de-xima/fc-form-designer" {
   export interface IEditorMap {
     [MultiSelectType]: {
       /** Stores the {@link IMultiSelectDescriptor }. */
       descriptor: IMultiSelectDescriptor;
+
       /** Stores the {@link MultiSelect }-Editor itself. */
       editor: MultiSelect;
+
       /** Stores the selected standard configurations CSV. */
       value: string;
     };
   }
 }
+
 /**
  * A plain {@link Editors.BaseEditor <typeof MultiSelectType>} retrieving it's  available options to display out of
  * {@link window..CodbiPluginData.fileListing } storing the made selections.
@@ -27,22 +31,26 @@ declare module "@de-xima/fc-form-designer" {
 export class MultiSelect extends Editors.BaseEditor<typeof MultiSelectType> {
   /** Stores the container for this {@link MultiSelect }. */
   private readonly _element: HTMLDivElement;
+
   /**
    * Creates a new {@link MultiSelect } by retrieving the available options from
    * {@link window..CodbiPluginData.fileListing } and generating an appropriate {@link HTMLInputElement } for each
    * entry there. Each {@link HTMLInputElement } will trigger a **set-property** from {@link Callbacks } when
    * clicked.
    *
-   * @param config - Configuration for this editor. */
+   * @param config - Configuration for this editor.
+   */
   constructor(config: TEditorCfg<IMultiSelectDescriptor>) {
     // Note: the second argument is deprecated, we pass the empty string
     super(config, "", "text");
+
     // #region Retrieve available standard configurations
     // biome-ignore lint/suspicious/noExplicitAny: TODO
     const listing = JSON.parse((window as any).CodbiPluginData.fileListing).map((file: string) => {
       return file.lastIndexOf(".") !== -1 ? file.substring(0, file.lastIndexOf(".")) : file;
     });
     // #endregion Retrieve available standard configurations
+
     // #region Generate and inject appropriate <input>s and <labels> for them.
     this._element = document.createElement("div");
     this._element.style.whiteSpace = "nowrap";
@@ -55,16 +63,9 @@ export class MultiSelect extends Editors.BaseEditor<typeof MultiSelectType> {
       newElement.setAttribute("id", `CodBi-StandardConfigSelector_${configuration}`);
       newElement.setAttribute("type", "checkbox");
       newElement.setAttribute("value", configuration);
-      newElement.addEventListener("click", (event) => {
-        const newValue = new Array<string>();
-
-        for (const element of this._element.querySelectorAll("input")) {
-          if (element.checked) {
-            newValue.push(element.value);
-          }
-        }
+      newElement.addEventListener("click", () => {
         // Propagate the change.
-        Callbacks["set-property"].fire(this.property, newValue.join(), this);
+        Callbacks["set-property"].fire(this.config.property, this.getValue(), this);
       });
 
       newLabel.setAttribute("for", `CodBi-StandardConfigSelector_${configuration}`);
@@ -75,25 +76,28 @@ export class MultiSelect extends Editors.BaseEditor<typeof MultiSelectType> {
       // #endregion Generate appropriate <input>s and <labels> for them.
     }
   }
+
   /** See {@link Editors.BaseEditor }'s **getElement**. */
   override getElement(): JQuery {
     return $(this._element);
   }
+
   /**
    * Generates a CSV of all selected standard configurations.
    *
-   * @returns A CSV of all selected standard configurations. */
+   * @returns A CSV of all selected standard configurations.
+   */
   override getValue(): string {
-    const result = Array<string>();
-
+    const result: string[] = [];
     for (const current of this._element.querySelectorAll("input")) {
       if (current.checked) {
-        result.push(current.innerHTML);
+        result.push(current.value);
       }
     }
 
     return result.join();
   }
+
   /**
    * Clears all selections prior to setting the selected configurations according to the received **data**.
    *
@@ -104,11 +108,11 @@ export class MultiSelect extends Editors.BaseEditor<typeof MultiSelectType> {
       element.checked = false;
     }
     // #endregion Clear selection.
-    // #region Set according to [data].
-    for (const standard of (data as string).split(",")) {
-      const element: HTMLInputElement = this._element.querySelector(`[value="${standard.trim()}"]`) as HTMLInputElement;
 
-      if (element) {
+    // #region Set according to [data].
+    for (const standard of parseString(data).split(",")) {
+      const element = this._element.querySelector(`[value="${standard.trim()}"]`);
+      if (element instanceof HTMLInputElement) {
         element.checked = true;
       }
     }

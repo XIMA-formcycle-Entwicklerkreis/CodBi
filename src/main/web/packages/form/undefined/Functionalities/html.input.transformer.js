916084978c24ec855e423719ec572526ed8691d2
Salvatore Callari <Callari@WaXCode.net>
2026-03-29 21:11:21 +0200

    chore: untrack generated files, update .gitignore and .gitattributes

--- src/main/web/packages/form/undefined/Functionalities/html.input.transformer.js
+++
@@ -1,5 +0,0 @@
-import { HTML_Input_Transformer } from "./chunk-2KATTLXM.js";
-import "./chunk-ZTMSSRTV.js";
-import "./chunk-AOJQKO6T.js";
-export { HTML_Input_Transformer };
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFtdLAogICJzb3VyY2VzQ29udGVudCI6IFtdLAogICJtYXBwaW5ncyI6ICIiLAogICJuYW1lcyI6IFtdCn0K

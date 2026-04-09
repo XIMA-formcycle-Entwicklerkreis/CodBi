ab13ac58d1456236ebcfb348dcebfb5a5dc53086
Salvatore Callari <Callari@WaXCode.net>
2026-04-09 20:18:54 +0200

    chore: move docs to gh-pages branch, untrack from main

--- docs/designer/it/assets/hierarchy.js
+++
@@ -1,1 +0,0 @@
-window.hierarchyData = "eJyNjbEKwjAYhN/l5r+1Nov8u6MgCC6lQ0iiDaYJJHEqeXcJhdZJXI7j+I5vQQwhJ/DQi64bCdE8nFHZBp/AC0QNL2cDxvl6kV4+TQThZb0GH/sT4R0dGMrJlEw6bFC7tynPDrQiYOSkm/pv1qEQqvtLdLv/Idqgdm+/RQQ1Waej8eBBjKWUD44gUrk="
